1fc16d8c81eefd0c73c5ef8de7aca5688c3998c5
Karol Soppa <karol.soppa50@gmail.com>
2025-04-12 00:51:04 +0200

    added interface to add some words to WORDS variable

--- main.sh
+++ main.sh
@@ -1,3 +1,10 @@
 PLIK="/usr/share/dict/words"
 mapfile -t DATA < "$PLIK"
-echo "${DATA[@]}"
+#echo "${DATA[@]}"
+if [ "$#" -eq 0 ]; then
+    echo "Podaj slowa linijak po linijce. Jak chcesz zakonczyc nacisnij ctrl+D:"
+    mapfile -t WORDS
+else
+    WORDS=("$@")
+fi
+
