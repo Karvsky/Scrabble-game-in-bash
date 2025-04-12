ffde1aae0f3ca8c06db27825b4aa93af731ec7c1
Karol Soppa <karol.soppa50@gmail.com>
2025-04-12 12:56:38 +0200

    added abbility to have own dictionary

--- main.sh
+++ main.sh
@@ -1,8 +1,24 @@
 PLIK="/usr/share/dict/words"
 mapfile -t DATA < "$PLIK"
 #echo "${DATA[@]}"
+variable="nothing"
+echo "Jeśli chcesz użyć własnej biblioteki wpisz TAK, natomiast jeśli chcesz użyć wbudowanej biblioteki wpisz NIE: "
+read VARIABLE
+while [[ "$VARIABLE" != "TAK" && "$VARIABLE" != "NIE" ]];
+do
+    echo "Podales zla mozliwa odpowiedz. Napisz jeszcze raz: "
+    read VARIABLE
+done
+
+if [ "$VARIABLE" = "TAK" ]; then
+    echo "Podaj sciezke do twojego slownika: "
+    read PLIK
+    mapfile -t DATA < "$PLIK" #mozna dodac pozniej opcje sprawdzenia czy uzytkownik nie popelnil bledu przy wpisywaniu
+    #nie sprawdzalem jeszcze czy dziala dodawanie wlasnej sciezki
+fi
+
 if [ "$#" -eq 0 ]; then
-    echo "Podaj slowa linijak po linijce. Jak chcesz zakonczyc nacisnij ctrl+D:"
+    echo "Podaj slowa linijka po linijce. Jak chcesz zakonczyc nacisnij ctrl+D:"
     mapfile -t WORDS
 else
     WORDS=("$@")
@@ -18,4 +34,5 @@ for i in "${WORDS[@]}"; do
         fi
     done
 done
+
 
